2565333ebe3ab3df01295d7034d96bf671361044
Farshid-997 <arnob.farshid@gmail.com>
2022-01-19 02:14:48 +0600

    commited

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ async function run(){
     const  serviceCollection=database.collection('services');
     const orderCollection=database.collection('orders')
     const  blogCollection=database.collection('Blogs')
+    const  contactCollection=database.collection('contact')
+    const usersCollection=database.collection('users')
     //get api
     
     app.get('/services',async(req,res)=>{
@@ -51,13 +53,26 @@ async function run(){
     
     app.post('/services',async(req,res)=>{
        
-        const service=req.body;
-        console.log('hit the post api',service)
+   
+      const place=req.body.place
+      const description=req.body.description
+      const price=req.body.price
+      const image=req.files.image
+      const imageData=image.data
+      const encodedPic=imageData.toString('base64')
+      const imgBuffer=Buffer.from(encodedPic,'base64')
       
-         const result= await serviceCollection.insertOne(service)
-         console.log(result)
-         res.json(result)
+      const services ={
+        place,
+        description,
+        price,
+        image:imgBuffer
       
+      }
+        
+      const result= await serviceCollection.insertOne(services)
+      console.log(result)
+      res.json(result)
        // res.send('post hitted')
     })
     
@@ -111,10 +126,74 @@ res.json(result)
 })
 
 app.get('/blog',async(req,res)=>{
-  const cursor=blogCollection.fins({})
+  const cursor=blogCollection.find({})
   const blogs= await cursor.toArray()
   res.json(blogs)
 })
+
+
+app.post('/contact',async(req,res)=>{
+  const contact=req.body
+  const result= await contactCollection.insertOne(contact)
+  res.json(result)
+})
+
+
+ //get the user 
+  
+//  app.get('/users/:email',async(req,res)=>{
+//   const email=req.params.email;
+//   const query={email:email}
+//   const users=await usersCollection.findOne(query)
+//  let isAdmin=false
+//  if(users?.role==='admin'){
+// isAdmin=true;
+//  }
+//   res.json({admin:isAdmin})
+// })
+  
+
+// //users post 
+// app.post('/users',async(req,res)=>{
+     
+//   const users=req.body;
+//   console.log('hit the post api',users)
+
+//    const result= await usersCollection.insertOne(users)
+//    console.log(result)
+//    res.json(result)
+
+//  // res.send('post hitted')
+// })
+
+// app.put('/users',async(req,res)=>{
+     
+// const users=req.body;
+// const filter={email:users.email}
+// const options={upsert:true}
+// const updateDoc={$set:users}
+
+//  const result= await usersCollection.updateOne(filter,updateDoc,options)
+//  console.log(result)
+//  res.json(result)
+
+// // res.send('post hitted')
+// })
+
+
+// app.put('/users/admin',async(req,res)=>{
+     
+// const users=req.body;
+// const filter={email:users.email}
+// const options={upsert:true}
+// const updateDoc={$set:{role:'admin'}}
+
+//  const result= await usersCollection.updateOne(filter,updateDoc,options)
+//  console.log(result)
+//  res.json(result)
+
+// // res.send('post hitted')
+// })
 
 
 
